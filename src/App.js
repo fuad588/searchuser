f2d124ed95e2fdafbff1c8bb1dfe89baf531556c
fuad588 <ffuadaliyev@gmail.com>
2023-01-07 10:28:02 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './App.css'
-import Users from './components/Users/Users'
+import React from "react";
+import "./App.css";
+import Users from "./components/Users/Users.js";
 export default function App() {
+  let items = []
+ 
+    fetch("https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood")
+      .then((response) => response.json())
+      .then((data) =>{ 
+        data.map((i)=>{
+          items.push(i)
+        })
+      });
+
   return (
-    <div className='App'>
-      <Users/>
+    <div className="App">
+      <Users item={items} />
     </div>
-  )
+  );
 }
